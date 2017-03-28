fc1b131524015073f1bb13e9fd9d6f5d4de45593
raymondproduction <raymondproduction@narod.ru>
2017-03-29 02:12:40 +0300

    changed monitor

--- for_my_work/monitor.js
+++ for_my_work/monitor.js
@@ -1,10 +1,25 @@
 var fs = require('fs');
 
-fs.watch('test.log', function(curr, prev) {
+var a = parametr('-a');
+var log = parametr('-log');
+
+if (!a) {
+  a = '127.0.0.1:8080';
+};
+
+if (!log) {
+  console.log('log file unknown');
+} else {
+  console.log('log file: ',log);
+};
+
+
+fs. (log, function(curr, prev) {
   // on file change we can read the new xml
   // console.log(`the current mtime is: ${curr.mtime}`);
   // console.log(`the previous mtime was: ${prev.mtime}`);
-  fs.readFile('test.log', 'utf8', function(err, data) {
+  console.log('change');
+  fs.readFile(log, 'utf8', function(err, data) {
     if (err) throw err;
     var strings = data.split('\n'); // разделить на строки
     // удаляем строки пустые строки в конце
@@ -15,12 +30,50 @@ fs.watch('test.log', function(curr, prev) {
       }
     }
     if (strings.length > 1) {
-      console.log('=>', strings[strings.length - 2]);
-      console.log('=>', strings[strings.length - 1]);
+      parseAndGetForSpeech(strings[strings.length - 1]);
     }
   });
 });
 
-2017-03-28 21:14:32
+function parseAndGetForSpeech(st) {
+  var arr = st.split(' ... ');
+  if (arr) {
+    var MAC = arr[3].split('-')[1].replace(' ', '');
+    var login = arr[1];
+    var flat = arr[2].split('-')[1].replace(' ', '');
+    var UIDAbills = arr[0];
 
+    if (MAC != '99:99:99:99:99:99') {
+      var st2 = [
+        'Пытается авторизироватся клиент с лоогиином',
+        login,
+        'жиивёт в квартире с номером',
+        flat,
+        'Номер клиента в абббилсе',
+        UIDAbills,
+        'Проганите ввланчиик, выставьте группу лентяиии.'
+      ].join(' ');
 
+      // GET запрос на сервер
+      var http = require('http');
+      var url = 'http://'+a+'/speak/' + encodeURI(st2);
+
+      http.get(url, function(res) {
+        res.setEncoding("utf8");
+        res.on("data", function(data) {
+          console.log(data);
+        });
+      });
+    };
+  }
+};
+
+function parametr(par) {
+  var res;
+  process.argv.forEach(function(item, i, arr) {
+    if (par == item) {
+      res = process.argv[i + 1];
+    }
+  });
+  return res;
+}
