676a4dd4a59d6868ab34fe2182b3af3d9033d10f
raymondproduction <raymondproduction@narod.ru>
2017-04-01 10:48:49 +0300

    Modified

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var restify = require('restify');
-var cmd = require('node-cmd');
+var speech = require('./speech.js');
 var bodyParser = require('body-parser');
 
 function getRespond(req, res, next) {
-  cmd.run('echo "'+req.params.speech+'" | festival --tts --language russian');
+  speech.speak(req.params.speech);
   res.send('You say: ' + req.params.speech);
   console.log('You say: ' + req.params.speech);
   next();
 }
 
 function postRespond(req, res, next) {
-  cmd.run('echo Это пост запрос. "'+
-    req.body.speech+
-    '" | festival --tts --language russian');
-
+  speech.speak('Это пост запрос. '+req.body.speech);
   res.send('You say: ' + req.body.speech);
   console.log(req.body);
-
   next();
 }
 
